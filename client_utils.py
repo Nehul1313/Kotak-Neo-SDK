b54b36ca377df8b32e96b306ff55654e4ebccee9
Nehul Patel <nehul1313patel@gmail.com>
2025-12-22 01:13:05 +0530

    SS-0001: Websocket testing

--- client_utils.py
+++ client_utils.py
@@ -10,7 +10,7 @@ consumer_key = os.getenv("consumer_key")
 mobile_number = os.getenv("mobile_number")
 ucc = os.getenv("ucc")
 mpin = os.getenv("mpin")
-
+latest_tick = None
 class ClientSetup:
     def __init__(self):
         self.consumer_key = consumer_key
@@ -25,9 +25,47 @@ class ClientSetup:
         self.client.totp_validate(mpin=self.mpin)
         
         # Setup Callbacks for websocket events (Optional)
-        self.client.on_message = lambda message: print(message)  # called when message is received from websocket
+        self.client.on_message = ClientSetup.on_message  # called when message is received from websocket
         self.client.on_error = lambda error_message: print(error_message)  # called when any error or exception occurs in code or websocket
         self.client.on_close = lambda message: print(message)  # called when websocket connection is closed
         self.client.on_open = lambda message: print(message)  # called when websocket successfully connects
-
+        
+        self.subscribe_channels()
+        
         return self.client
+        
+    @staticmethod
+    def on_message(msg):
+        global latest_tick
+
+        # Filter only real trade ticks
+        data = msg.get("data", [])
+        if not data: return
+
+        d = data[0]
+        if "ltp" not in d: return
+
+        # Normalize once
+        latest_tick = {
+            "token": d["tk"],
+            "exchange": d["e"],
+            "ltp": float(d["ltp"]),
+            "volume": int(d.get("v", 0)),
+            "timestamp": d.get("ltt", d.get("fdtm"))
+        }
+        
+    def subscribe_channels(self):
+        # Non_index Sockets
+        instrument_tokens = [
+            {"instrument_token": "486608", "exchange_segment": "mcx_fo"}, #CRUDEOIL JAN 
+            ]
+        
+        self.client.subscribe(instrument_tokens=instrument_tokens,isIndex=False,isDepth=False)
+
+        #Index Sockets
+        instrument_tokens = [
+            {"instrument_token": "Nifty 50", "exchange_segment": "nse_cm"},
+            ]
+        
+        self.client.subscribe(instrument_tokens=instrument_tokens,isIndex=True,isDepth=False)
+
---
+++ web_socket.py
@@ -0,0 +1,9 @@
+# Non_index Sockets
+instrument_tokens = [
+    {"instrument_token": "486608", "exchange_segment": "mcx_fo"}, #CRUDEOIL JAN 
+]
+
+#Index Sockets
+instrument_tokens = [
+    {"instrument_token": "Nifty 50", "exchange_segment": "nse_cm"},
+]
